ef1babbf7ca3f30199f2fc09b857a64f85dbe7fb
rmbo0o <rmboo25@gmail>
2025-09-28 13:43:42 -0700

    change the database

--- config/db.js
+++ config/db.js
@@ -1,40 +1,29 @@
-// const mysql = require('mysql2');
 const mysql = require('mysql2/promise');
 const dotenv = require('dotenv');
 
 dotenv.config();
-
-// const connection = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   port: process.env.DB_PORT || 3306,
-// });
-
-
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
-
-const connectDB = () => {
-  pool.connect((err) => {
-    if (err) {
-      console.error('Error connecting to MySQL:', err);
-      process.exit(1);
-    } else {
-      console.log('MySQL connected');
-    }
-  });
+// Test connection
+const connectDB = async () => {
+  try {
+    const connection = await pool.getConnection();
+    console.log('MySQL connected');
+    connection.release();
+  } catch (err) {
+    console.error('Error connecting to MySQL:', err);
+    process.exit(1);
+  }
 };
 
-module.exports = { connectDB, pool};
+module.exports = { connectDB, pool };
