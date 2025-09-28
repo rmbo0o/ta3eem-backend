2461e0d4856e60420de4ca2fa8b9831156c4242d
rmbo0o <rmboo25@gmail>
2025-09-28 13:32:49 -0700

    change the database

--- config/db.js
+++ config/db.js
@@ -3,13 +3,27 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const connection = mysql.createConnection({
+// const connection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT || 3306,
+// });
+
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
+
 
 const connectDB = () => {
   connection.connect((err) => {
@@ -22,4 +36,4 @@ const connectDB = () => {
   });
 };
 
-module.exports = { connectDB, connection };
+module.exports = { connectDB, connection ,pool};
